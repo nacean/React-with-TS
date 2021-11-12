e6985679948ec627d64118e1950b4f3a5c432245
nacean <nacean@naver.com>
2021-11-13 02:51:57 +0900

    Have Error Calculator Test

--- test/Calculator.test.tsx
+++ test/Calculator.test.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CalculatorButtons from "../src/component/CalculatorButtons";
 import CalculatorResult from "../src/component/CalculatorResult";
+import userEvent from "@testing-library/user-event";
 
 describe("Calculator Test", () => {
-  const ButtonComponent = render(<CalculatorButtons />);
-  const ResultComponent = render(<CalculatorResult />);
-  it("Calculator Complete", () => {
-    const Button_1 = ButtonComponent.container.querySelector("#1");
-    const Button_2 = ButtonComponent.container.querySelector("#2");
-    fireEvent.click(Button_1);
-    fireEvent.click(Button_2);
-    const Result = ResultComponent.container.getElementsByClassName("Result");
-    expect(Result).toBe("12");
+  test("Calculator Complete", () => {
+    render(
+      <div>
+        <CalculatorButtons />
+        <CalculatorResult />
+      </div>
+    );
+    userEvent.click(screen.getByText("1"));
+    userEvent.click(screen.getByText("2"));
+    expect(screen.getByText("12").innerText).toBe("12");
   });
 });
